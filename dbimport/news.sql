a1118aca7f28f9e04361c61f50480b62226ee7d7
Georg Ringer <georg.ringer@gmail.com>
2013-07-06 20:32:31 +0200

    Create news.sql

---
+++ dbimport/news.sql
@@ -0,0 +1,309 @@
+--
+-- Table structure for table `tx_news_domain_model_category`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_category`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_category` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `tstamp` int(11) NOT NULL DEFAULT '0',
+  `crdate` int(11) NOT NULL DEFAULT '0',
+  `cruser_id` int(11) NOT NULL DEFAULT '0',
+  `sys_language_uid` int(11) NOT NULL DEFAULT '0',
+  `l10n_parent` int(11) NOT NULL DEFAULT '0',
+  `l10n_diffsource` mediumtext,
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) NOT NULL DEFAULT '0',
+  `starttime` int(11) NOT NULL DEFAULT '0',
+  `endtime` int(11) NOT NULL DEFAULT '0',
+  `fe_group` varchar(100) NOT NULL DEFAULT '0',
+  `title` tinytext,
+  `description` text,
+  `image` text,
+  `parentcategory` int(11) NOT NULL DEFAULT '0',
+  `single_pid` int(11) unsigned NOT NULL DEFAULT '0',
+  `shortcut` int(11) NOT NULL DEFAULT '0',
+  `import_id` varchar(100) NOT NULL DEFAULT '',
+  `import_source` varchar(100) NOT NULL DEFAULT '',
+  `is_dummy_record` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uid`),
+  KEY `parent` (`pid`),
+  KEY `parentcategory` (`parentcategory`),
+  KEY `import` (`import_id`,`import_source`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_file`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_file`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_file` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `tstamp` int(11) NOT NULL DEFAULT '0',
+  `crdate` int(11) NOT NULL DEFAULT '0',
+  `cruser_id` int(11) NOT NULL DEFAULT '0',
+  `sys_language_uid` int(11) NOT NULL DEFAULT '0',
+  `l10n_parent` int(11) NOT NULL DEFAULT '0',
+  `l10n_diffsource` mediumtext,
+  `t3ver_oid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_id` int(11) NOT NULL DEFAULT '0',
+  `t3_origuid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_wsid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_label` varchar(30) NOT NULL DEFAULT '',
+  `t3ver_state` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_stage` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_count` int(11) NOT NULL DEFAULT '0',
+  `t3ver_tstamp` int(11) NOT NULL DEFAULT '0',
+  `sorting` int(10) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) NOT NULL DEFAULT '0',
+  `fe_group` varchar(100) NOT NULL DEFAULT '0',
+  `title` tinytext,
+  `description` text,
+  `file` text,
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `is_dummy_record` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uid`),
+  KEY `parent` (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_link`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_link`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_link` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `tstamp` int(11) NOT NULL DEFAULT '0',
+  `crdate` int(11) NOT NULL DEFAULT '0',
+  `cruser_id` int(11) NOT NULL DEFAULT '0',
+  `sys_language_uid` int(11) NOT NULL DEFAULT '0',
+  `l10n_parent` int(11) NOT NULL DEFAULT '0',
+  `l10n_diffsource` mediumtext,
+  `t3ver_oid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_id` int(11) NOT NULL DEFAULT '0',
+  `t3_origuid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_wsid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_label` varchar(30) NOT NULL DEFAULT '',
+  `t3ver_state` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_stage` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_count` int(11) NOT NULL DEFAULT '0',
+  `t3ver_tstamp` int(11) NOT NULL DEFAULT '0',
+  `sorting` int(10) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) NOT NULL DEFAULT '0',
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `title` tinytext,
+  `description` text,
+  `uri` text,
+  `is_dummy_record` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uid`),
+  KEY `parent` (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_media`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_media`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_media` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `tstamp` int(11) NOT NULL DEFAULT '0',
+  `crdate` int(11) NOT NULL DEFAULT '0',
+  `cruser_id` int(11) NOT NULL DEFAULT '0',
+  `sys_language_uid` int(11) NOT NULL DEFAULT '0',
+  `l10n_parent` int(11) NOT NULL DEFAULT '0',
+  `t3_origuid` int(11) NOT NULL DEFAULT '0',
+  `l10n_diffsource` mediumtext,
+  `t3ver_oid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_id` int(11) NOT NULL DEFAULT '0',
+  `t3ver_wsid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_label` varchar(30) NOT NULL DEFAULT '',
+  `t3ver_state` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_stage` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_count` int(11) NOT NULL DEFAULT '0',
+  `t3ver_tstamp` int(11) NOT NULL DEFAULT '0',
+  `sorting` int(10) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) NOT NULL DEFAULT '0',
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `caption` tinytext,
+  `alt` tinytext,
+  `title` tinytext,
+  `copyright` tinytext,
+  `image` text,
+  `html` text,
+  `dam` int(11) NOT NULL DEFAULT '0',
+  `multimedia` tinytext,
+  `type` int(11) NOT NULL DEFAULT '0',
+  `showinpreview` tinyint(4) NOT NULL DEFAULT '0',
+  `width` int(11) NOT NULL DEFAULT '0',
+  `height` int(11) NOT NULL DEFAULT '0',
+  `description` mediumtext,
+  `is_dummy_record` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uid`),
+  KEY `parent` (`pid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_news`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_news`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_news` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `tstamp` int(11) NOT NULL DEFAULT '0',
+  `crdate` int(11) NOT NULL DEFAULT '0',
+  `cruser_id` int(11) NOT NULL DEFAULT '0',
+  `t3ver_oid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_id` int(11) NOT NULL DEFAULT '0',
+  `t3ver_wsid` int(11) NOT NULL DEFAULT '0',
+  `t3ver_label` varchar(30) NOT NULL DEFAULT '',
+  `t3ver_state` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_stage` tinyint(4) NOT NULL DEFAULT '0',
+  `t3ver_count` int(11) NOT NULL DEFAULT '0',
+  `t3ver_tstamp` int(11) NOT NULL DEFAULT '0',
+  `t3_origuid` int(11) NOT NULL DEFAULT '0',
+  `editlock` tinyint(4) NOT NULL DEFAULT '0',
+  `sys_language_uid` int(11) NOT NULL DEFAULT '0',
+  `l10n_parent` int(11) NOT NULL DEFAULT '0',
+  `l10n_diffsource` mediumtext,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) NOT NULL DEFAULT '0',
+  `starttime` int(11) NOT NULL DEFAULT '0',
+  `endtime` int(11) NOT NULL DEFAULT '0',
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  `fe_group` varchar(100) NOT NULL DEFAULT '0',
+  `title` tinytext,
+  `teaser` text,
+  `bodytext` mediumtext,
+  `datetime` int(11) NOT NULL DEFAULT '0',
+  `archive` int(11) NOT NULL DEFAULT '0',
+  `author` tinytext,
+  `author_email` tinytext,
+  `categories` int(11) NOT NULL DEFAULT '0',
+  `related` int(11) NOT NULL DEFAULT '0',
+  `related_from` int(11) NOT NULL DEFAULT '0',
+  `related_files` tinytext,
+  `related_links` tinytext,
+  `type` varchar(100) DEFAULT '0',
+  `keywords` text,
+  `tags` int(11) NOT NULL DEFAULT '0',
+  `media` text,
+  `internalurl` text,
+  `externalurl` text,
+  `istopnews` int(11) NOT NULL DEFAULT '0',
+  `content_elements` text,
+  `path_segment` tinytext,
+  `alternative_title` tinytext,
+  `rte_disabled` tinyint(4) NOT NULL DEFAULT '0',
+  `import_id` varchar(100) NOT NULL DEFAULT '',
+  `import_source` varchar(100) NOT NULL DEFAULT '',
+  `is_dummy_record` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uid`),
+  KEY `parent` (`pid`),
+  KEY `import` (`import_id`,`import_source`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_news_category_mm`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_news_category_mm`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_news_category_mm` (
+  `uid_local` int(11) NOT NULL DEFAULT '0',
+  `uid_foreign` int(11) NOT NULL DEFAULT '0',
+  `tablenames` varchar(30) NOT NULL DEFAULT '',
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  KEY `uid_local` (`uid_local`),
+  KEY `uid_foreign` (`uid_foreign`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_news_related_mm`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_news_related_mm`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_news_related_mm` (
+  `uid_local` int(11) NOT NULL DEFAULT '0',
+  `uid_foreign` int(11) NOT NULL DEFAULT '0',
+  `tablenames` varchar(30) NOT NULL DEFAULT '',
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  `sorting_foreign` int(11) NOT NULL DEFAULT '0',
+  KEY `uid_local` (`uid_local`),
+  KEY `uid_foreign` (`uid_foreign`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_news_tag_mm`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_news_tag_mm`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_news_tag_mm` (
+  `uid_local` int(11) NOT NULL DEFAULT '0',
+  `uid_foreign` int(11) NOT NULL DEFAULT '0',
+  `tablenames` varchar(30) NOT NULL DEFAULT '',
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  KEY `uid_local` (`uid_local`),
+  KEY `uid_foreign` (`uid_foreign`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_news_ttcontent_mm`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_news_ttcontent_mm`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_news_ttcontent_mm` (
+  `uid_local` int(11) NOT NULL DEFAULT '0',
+  `uid_foreign` int(11) NOT NULL DEFAULT '0',
+  `tablenames` varchar(30) NOT NULL DEFAULT '',
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  KEY `uid_local` (`uid_local`),
+  KEY `uid_foreign` (`uid_foreign`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tx_news_domain_model_tag`
+--
+
+DROP TABLE IF EXISTS `tx_news_domain_model_tag`;
+CREATE TABLE IF NOT EXISTS `tx_news_domain_model_tag` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `tstamp` int(11) NOT NULL DEFAULT '0',
+  `crdate` int(11) NOT NULL DEFAULT '0',
+  `cruser_id` int(11) NOT NULL DEFAULT '0',
+  `sorting` int(11) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `hidden` tinyint(4) NOT NULL DEFAULT '0',
+  `title` tinytext,
+  PRIMARY KEY (`uid`),
+  KEY `parent` (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
